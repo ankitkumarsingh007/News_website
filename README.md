f3c8490893872a29808e192b99b5f405d809b821
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2021-12-25 00:00:00 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+This website takes data from a NEWS api and publishes it in a given formet.
+
+features :
+(1)Top news sorted by relevance.
+(2)You can further read news if intrested.
+(3)You can search keytopics in search bar.
---
+++ app.js
@@ -0,0 +1,57 @@
+console.log("This is my index js file");
+
+// Grab the news container
+let newsAccordion = document.getElementById('newsAccordion');
+
+// Create an ajax get request
+const xhr = new XMLHttpRequest();
+xhr.open('GET', `https://newsapi.org/v2/everything?q=keyword&apiKey=71e7f3cd9e654d269bd93718ca6079e6`, true);
+
+// What to do when response is ready
+xhr.onload = function() {
+    if (this.status === 200) {
+        let json = JSON.parse(this.responseText);
+        let articles = json.articles;
+        console.log(articles);
+        let newsHtml = "";
+        articles.forEach(function(element, index) {
+            // console.log(element, index)
+            let news = `<div class="card">
+                            <div class="card-header" id="heading${index}">
+                                <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
+                                    aria-expanded="false" aria-controls="collapse${index}">
+                                   <b>Breaking News ${index+1}:</b> ${element["title"]}
+                                </button>
+                                </h2>
+                            </div>
+
+                            <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsAccordion">
+                                <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
+                            </div>
+                        </div>`;
+            newsHtml += news;
+        });
+        newsAccordion.innerHTML = newsHtml;
+    } else {
+        console.log("Some error occured")
+    }
+}
+
+xhr.send()
+
+let search = document.getElementById('searchTxt');
+search.addEventListener("input", function() {
+
+    let inputVal = search.value.toLowerCase();
+    let card = document.getElementsByClassName('card');
+    Array.from(card).forEach(function(element) {
+        let cardTitle = element.getElementsByTagName("button")[0].innerText.toLowerCase();
+        //here any other query selector is not working efficiently only tag is working why??
+        if (cardTitle.includes(inputVal)) {
+            element.style.display = "block";
+        } else {
+            element.style.display = "none";
+        }
+    })
+})
